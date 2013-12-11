e6bb5e370bda60a4d508715f5f7e4f54f89ff9b7
mmontone <marianomontone@gmail.com>
2013-12-10 22:57:01 -0300

    Detect selected item in navigation tree

--- configurator.py
+++ configurator.py
@@ -106,6 +106,9 @@ class ConfigurationSchema:
     def sections(self):
         return self._sections.values()
     
+    def get_section(self, name):
+        return self._sections[name]
+    
     def parents(self):
         return self._parents
     
@@ -223,6 +226,10 @@ class ConfigurationSchemaNavigator(tk.Frame):
         
         for schema in schemas:
             self.insert_schema(schema)
+            
+        self.tree.tag_bind('schema', '<ButtonRelease-1>', self.select_schema)
+        self.tree.tag_bind('section', '<ButtonRelease-1>', self.select_section)
+        self.tree.tag_bind('option', '<ButtonRelease-1>', self.select_option)
         
         pane.add(self.tree)
         
@@ -232,18 +239,61 @@ class ConfigurationSchemaNavigator(tk.Frame):
         pane.add(self.editor)
         pane.pack()
         
+    def select_schema(self, ev):
+        item_id = str(self.tree.focus())
+        schema = self.find_schema(item_id)
+        print 'Selected schema was %s' % item_id
+        print schema
+        
+    def find_schema(self, id):
+        return next((sc for sc in self.schemas if sc.name == id), None)
+
+    def select_section(self, ev):
+        item_id = str(self.tree.focus())
+        print 'Selected section was %s' % item_id
+        section = self.find_section(item_id)
+        print section
+        
+    def find_section(self, id):
+        path = id.split('.')
+        # First element is the schema, find it first
+        schema = self.find_schema(path[0])
+        
+        section = schema
+        i = 1
+        while i <= len(path) - 1:
+            section = section.get_section(path[i])
+            i = i + 1
+        
+        return section
+    
+    def select_option(self, ev):
+        item_id = str(self.tree.focus())
+        print 'Selected option was %s' % item_id
+        print self.find_option(item_id)
+        
+    def find_option(self, id):
+        path= id.split('.')
+        schema = self.find_schema(path[0])
+        section = schema
+        i = 1
+        while i <= len(path) - 2:
+            section = section.get_section(path[i])
+            i = i + 1
+        return section.get_option(path[len(path) - 1])
+        
     def insert_schema(self, schema):
-        sc = self.tree.insert('', 'end', schema.name, text=schema.name)
+        sc = self.tree.insert('', 'end', schema.name, text=schema.name, tags='schema')
         for section in schema.sections():
             self.insert_section(section, sc)
             
     def insert_section(self, section, parent):
-        sc = self.tree.insert(parent, 'end', parent + '.' + section.name, text=section.name)
+        sc = self.tree.insert(parent, 'end', parent + '.' + section.name, text=section.name, tags='section')
         for subsection in section.subsections():
             self.insert_section(subsection, sc)
         
         for option in section.options():
-            self.tree.insert(sc, 'end', sc + '.' + option.name, text=option.name)
+            self.tree.insert(sc, 'end', sc + '.' + option.name, text=option.name, tags='option')
             
 class ConfigurationSchemaEditor(tk.Frame):
     def __init__(self, master, schema):
@@ -251,16 +301,21 @@ class ConfigurationSchemaEditor(tk.Frame):
         
         self.schema = schema
         tk.Label(self, text="Name: ").grid(row=0, column=0)
-        self.schema_name = tk.StringVar().set(schema.name or "")
+        self.schema_name = tk.StringVar()
+        self.schema_name.set(schema.name or "")
+        
         tk.Entry(self, textvariable=self.schema_name).grid(row=0, column=1)
         tk.Label(self, text="Parents:").grid(row=1, column=0)
         parents = DoubleListSelector(self, source=list_configuration_schemas(),
                                         selected=schema.parents())
         parents.grid(row=1, column=1)
+        
+        self.schema_documentation = schema.documentation
             
         tk.Label(self, text="Documentation").grid(row=2, column=0)
-        tk.Text(self).grid(row=2, column=1)
-        tk.Label(self, text="Options").grid(row=3,column=0)
+        text = tk.Text(self)
+        text.insert(tk.END, self.schema_documentation)
+        text.grid(row=2, column=1)
         
 class ConfigurationSchemaSectionEditor(tk.Frame):
     def __init__(self, master, section):
@@ -268,8 +323,7 @@ class ConfigurationSchemaSectionEditor(tk.Frame):
         
         self.section = section
         tk.Label(self, text="Name: ").grid(row=0, column=0)
-        tk.Entry()
-                
+        tk.Entry()                
            
 class ConfigurationNavigator(tk.Frame):
     def __init__(self, master, configs):
